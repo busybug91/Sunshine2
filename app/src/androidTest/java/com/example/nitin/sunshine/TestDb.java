99871222d6e306c5d57f66bc4f9fb3519f0f87fe
nahuja1 <nahuja1@asu.edu>
2014-08-21 16:25:23 -0700

    Content provider and test code
    Content provider support added.

--- app/src/androidTest/java/com/example/nitin/sunshine/TestDb.java
+++ app/src/androidTest/java/com/example/nitin/sunshine/TestDb.java
@@ -1,29 +1,158 @@
 package com.example.nitin.sunshine;
 
+import android.content.ContentValues;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.test.AndroidTestCase;
+import android.util.Log;
 
 import com.example.nitin.sunshine.data.WeatherDbHelper;
+import com.example.nitin.sunshine.data.WeatherContract.LocationEntry;
+import com.example.nitin.sunshine.data.WeatherContract.WeatherEntry;
 
 
-public class TestDb extends AndroidTestCase {
+import java.util.Map;
+import java.util.Set;
 
-    public static final String LOG_TAG = TestDb.class.getSimpleName();
+    public class TestDb extends AndroidTestCase {
 
-    public void testCreateDb() throws Throwable {
-        mContext.deleteDatabase(WeatherDbHelper.DATABASE_NAME);
-        SQLiteDatabase db = new WeatherDbHelper(
-                this.mContext).getWritableDatabase();
-        assertEquals(true, db.isOpen());
-        db.close();
+        public static final String LOG_TAG = TestDb.class.getSimpleName();
+
+        static public String TEST_CITY_NAME = "North Pole";
+        static public String TEST_LOCATION = "99705";
+        static public String TEST_DATE = "20141205";
+
+        static final String KALAMAZOO_LOCATION_SETTING = "kalamazoo";
+        static final String KALAMAZOO_WEATHER_START_DATE = "20140625";
+
+        public void testCreateDb() throws Throwable {
+            mContext.deleteDatabase(WeatherDbHelper.DATABASE_NAME);
+            SQLiteDatabase db = new WeatherDbHelper(
+                    this.mContext).getWritableDatabase();
+            assertEquals(true, db.isOpen());
+            db.close();
+        }
+
+        public void testInsertReadDb() {
+
+            // If there's an error in those massive SQL table creation Strings,
+            // errors will be thrown here when you try to get a writable database.
+            WeatherDbHelper dbHelper = new WeatherDbHelper(mContext);
+            SQLiteDatabase db = dbHelper.getWritableDatabase();
+
+            ContentValues testValues = createNorthPoleLocationValues();
+
+            long locationRowId;
+            locationRowId = db.insert(LocationEntry.TABLE_NAME, null, testValues);
+
+            // Verify we got a row back.
+            assertTrue(locationRowId != -1);
+            Log.d(LOG_TAG, "New row id: " + locationRowId);
+
+            // Data's inserted.  IN THEORY.  Now pull some out to stare at it and verify it made
+            // the round trip.
+
+            // A cursor is your primary interface to the query results.
+            Cursor cursor = db.query(
+                    LocationEntry.TABLE_NAME,  // Table to Query
+                    null, // all columns
+                    null, // Columns for the "where" clause
+                    null, // Values for the "where" clause
+                    null, // columns to group by
+                    null, // columns to filter by row groups
+                    null // sort order
+            );
+
+            validateCursor(cursor, testValues);
+
+            // Fantastic.  Now that we have a location, add some weather!
+            ContentValues weatherValues = createWeatherValues(locationRowId);
+
+            long weatherRowId = db.insert(WeatherEntry.TABLE_NAME, null, weatherValues);
+            assertTrue(weatherRowId != -1);
+
+            // A cursor is your primary interface to the query results.
+            Cursor weatherCursor = db.query(
+                    WeatherEntry.TABLE_NAME,  // Table to Query
+                    null, // leaving "columns" null just returns all the columns.
+                    null, // cols for "where" clause
+                    null, // values for "where" clause
+                    null, // columns to group by
+                    null, // columns to filter by row groups
+                    null  // sort order
+            );
+
+            validateCursor(weatherCursor, weatherValues);
+
+            dbHelper.close();
+        }
+
+        static ContentValues createWeatherValues(long locationRowId) {
+            ContentValues weatherValues = new ContentValues();
+            weatherValues.put(WeatherEntry.COLUMN_LOC_KEY, locationRowId);
+            weatherValues.put(WeatherEntry.COLUMN_DATETEXT, TEST_DATE);
+            weatherValues.put(WeatherEntry.COLUMN_DEGREES, 1.1);
+            weatherValues.put(WeatherEntry.COLUMN_HUMIDITY, 1.2);
+            weatherValues.put(WeatherEntry.COLUMN_PRESSURE, 1.3);
+            weatherValues.put(WeatherEntry.COLUMN_MAX_TEMP, 75);
+            weatherValues.put(WeatherEntry.COLUMN_MIN_TEMP, 65);
+            weatherValues.put(WeatherEntry.COLUMN_SHORT_DESC, "Asteroids");
+            weatherValues.put(WeatherEntry.COLUMN_WIND_SPEED, 5.5);
+            weatherValues.put(WeatherEntry.COLUMN_WEATHER_ID, 321);
+
+            return weatherValues;
+        }
+
+        static ContentValues createNorthPoleLocationValues() {
+            // Create a new map of values, where column names are the keys
+            ContentValues testValues = new ContentValues();
+            testValues.put(LocationEntry.COLUMN_LOCATION_SETTING, TEST_LOCATION);
+            testValues.put(LocationEntry.COLUMN_CITY_NAME, TEST_CITY_NAME);
+            testValues.put(LocationEntry.COLUMN_COORD_LAT, 64.7488);
+            testValues.put(LocationEntry.COLUMN_COORD_LONG, -147.353);
+
+            return testValues;
+        }
+
+        static ContentValues createKalamazooWeatherValues(long locationRowId) {
+            ContentValues weatherValues = new ContentValues();
+            weatherValues.put(WeatherEntry.COLUMN_LOC_KEY, locationRowId);
+            weatherValues.put(WeatherEntry.COLUMN_DATETEXT, KALAMAZOO_WEATHER_START_DATE);
+            weatherValues.put(WeatherEntry.COLUMN_DEGREES, 1.2);
+            weatherValues.put(WeatherEntry.COLUMN_HUMIDITY, 1.5);
+            weatherValues.put(WeatherEntry.COLUMN_PRESSURE, 1.1);
+            weatherValues.put(WeatherEntry.COLUMN_MAX_TEMP, 85);
+            weatherValues.put(WeatherEntry.COLUMN_MIN_TEMP, 35);
+            weatherValues.put(WeatherEntry.COLUMN_SHORT_DESC, "Cats and Dogs");
+            weatherValues.put(WeatherEntry.COLUMN_WIND_SPEED, 3.4);
+            weatherValues.put(WeatherEntry.COLUMN_WEATHER_ID, 42);
+
+            return weatherValues;
+        }
+
+        static ContentValues createKalamazooLocationValues() {
+            // Create a new map of values, where column names are the keys
+            ContentValues testValues = new ContentValues();
+            testValues.put(LocationEntry.COLUMN_LOCATION_SETTING, KALAMAZOO_LOCATION_SETTING);
+            testValues.put(LocationEntry.COLUMN_CITY_NAME, "Kalamazoo");
+            testValues.put(LocationEntry.COLUMN_COORD_LAT, 42.2917);
+            testValues.put(LocationEntry.COLUMN_COORD_LONG, -85.5872);
+
+            return testValues;
+        }
+
+        static void validateCursor(Cursor valueCursor, ContentValues expectedValues) {
+
+            assertTrue(valueCursor.moveToFirst());
+
+            Set<Map.Entry<String, Object>> valueSet = expectedValues.valueSet();
+            for (Map.Entry<String, Object> entry : valueSet) {
+                String columnName = entry.getKey();
+                int idx = valueCursor.getColumnIndex(columnName);
+                assertFalse(idx == -1);
+                String expectedValue = entry.getValue().toString();
+                assertEquals(expectedValue, valueCursor.getString(idx));
+            }
+            valueCursor.close();
+        }
     }
-    public void testInsertReadOperation()
-    {
-        //we can add code here to check if our insert and delete works or not.
-        //Simply create some values and add them to a Content Values class via Contract class.
-        //Insert these values into the database using a DbHelper.
-        //Read these values from Db. No need to go through each and every value.
-        //Just check that if the cursor has a row or not. If cursor is empty then that means our insert didn't work.
-
-    }
-}
--- app/src/main/java/com/example/nitin/sunshine/data/WeatherContract.java
+++ app/src/main/java/com/example/nitin/sunshine/data/WeatherContract.java
@@ -15,6 +15,8 @@
  */
 package com.example.nitin.sunshine.data;
 
+import android.content.ContentUris;
+import android.net.Uri;
 import android.provider.BaseColumns;
 
 /**
@@ -22,7 +24,26 @@ import android.provider.BaseColumns;
  */
 public class WeatherContract {
     /* Inner class that defines the table contents of the weather table */
+    //Content Authority
+    public static final String CONTENT_AUTHORITY="com.example.nitin.sunshine";
+
+    // This will be the base uri. All other specific uris will be built upon this one
+    public static final Uri BASE_CONTENT_URI=Uri.parse("content://"+CONTENT_AUTHORITY);
+
+    // next step in path to weather and location data
+    public static final String PATH_WEATHER="weather";
+    public static final String PATH_LOCATION="location";
+
     public static final class WeatherEntry implements BaseColumns {
+
+        public static final Uri CONTENT_URI =
+                BASE_CONTENT_URI.buildUpon().appendPath(PATH_WEATHER).build();
+
+        public static final String CONTENT_TYPE =
+                "vnd.android.cursor.dir/" + CONTENT_AUTHORITY + "/" + PATH_WEATHER;
+        public static final String CONTENT_ITEM_TYPE =
+                "vnd.android.cursor.item/" + CONTENT_AUTHORITY + "/" + PATH_WEATHER;
+
 
         public static final String TABLE_NAME = "weather";
 
@@ -52,9 +73,53 @@ public class WeatherContract {
 
         // Degrees are meteorological degrees (e.g, 0 is north, 180 is south).  Stored as floats.
         public static final String COLUMN_DEGREES = "degrees";
+
+            /*Below are some helper functions added from the gist*/
+
+
+        public static Uri buildWeatherUri(long id) {
+            return ContentUris.withAppendedId(CONTENT_URI, id);
+        }
+
+        public static Uri buildWeatherLocation(String locationSetting) {
+            return CONTENT_URI.buildUpon().appendPath(locationSetting).build();
+        }
+
+        public static Uri buildWeatherLocationWithStartDate(
+                String locationSetting, String startDate) {
+            return CONTENT_URI.buildUpon().appendPath(locationSetting)
+                    .appendQueryParameter(COLUMN_DATETEXT, startDate).build();
+        }
+
+        public static Uri buildWeatherLocationWithDate(String locationSetting, String date) {
+            return CONTENT_URI.buildUpon().appendPath(locationSetting).appendPath(date).build();
+        }
+
+        public static String getLocationSettingFromUri(Uri uri) {
+            return uri.getPathSegments().get(1);
+        }
+
+        public static String getDateFromUri(Uri uri) {
+            return uri.getPathSegments().get(2);
+        }
+
+        public static String getStartDateFromUri(Uri uri) {
+            return uri.getQueryParameter(COLUMN_DATETEXT);
+        }
+
     }
     /* Inner class that defines the table contents of the location table */
     public static final class LocationEntry implements BaseColumns {
+
+        public static final Uri CONTENT_URI =
+                BASE_CONTENT_URI.buildUpon().appendPath(PATH_LOCATION).build();
+
+
+        public static final String CONTENT_TYPE =
+                "vnd.android.cursor.dir/" + CONTENT_AUTHORITY + "/" + PATH_LOCATION;
+
+        public static final String CONTENT_ITEM_TYPE =
+                "vnd.android.cursor.item/" + CONTENT_AUTHORITY + "/" + PATH_LOCATION;
 
         // Table name
         public static final String TABLE_NAME = "location";
@@ -72,5 +137,15 @@ public class WeatherContract {
         // map intent, we store the latitude and longitude as returned by openweathermap.
         public static final String COLUMN_COORD_LAT = "coord_lat";
         public static final String COLUMN_COORD_LONG = "coord_long";
+
+        public static Uri buildLocationUri(long id) {
+            return ContentUris.withAppendedId(CONTENT_URI, id);
+        }
+
+
     }
+
+
+
+
 }
